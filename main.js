3875a8f1c806f75cc9e8122d8f6dd7b7a259b0b2
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-01-27 23:44:42 -0600

    Estructura semántica de la página de productos

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const products = document.getElementById("products");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const ALBUM_API = "https://jsonplaceholder.typicode.com/albums";
 
-setupCounter(document.querySelector('#counter'))
+const fetchData = async () => {
+    const response = await fetch(ALBUM_API);
+    const albums = await response.json();
+};
+
+fetchData();
